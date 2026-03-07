9bab70a51e44cd7122c058faa586614c15caad98
Kaua Felix <kfelixcosta11@gmail.com>
2026-03-06 21:41:32 -0300

    feat: adiciona implementacao de service

--- gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/service/ItemPedidoServiceImpl.java
+++ gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/service/ItemPedidoServiceImpl.java
@@ -1,10 +1,9 @@
 package com.weg.centroweg.gestaovendas.application.service;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UsuarioServiceImpl {
+public class ItemPedidoServiceImpl {
 }
