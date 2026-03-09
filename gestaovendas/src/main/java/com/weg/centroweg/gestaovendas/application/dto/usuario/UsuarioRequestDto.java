f2a928328b51ffe91dc121ed466d45a94e153afe
Kaua Felix <kfelixcosta11@gmail.com>
2026-03-09 12:00:59 -0300

    feat: adiciona validation para valor minimo na senha

--- gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/dto/usuario/UsuarioRequestDto.java
+++ gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/dto/usuario/UsuarioRequestDto.java
@@ -4,6 +4,7 @@ import com.weg.centroweg.gestaovendas.domain.entity.enums.RoleUsuario;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UsuarioRequestDto(
 
@@ -15,6 +16,7 @@ public record UsuarioRequestDto(
         String email,
 
         @NotBlank (message = "A senha do usuário é obrigatória")
+        @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
         String senha,
 
         @NotNull (message = "O papel do usuário é obrigatório")
