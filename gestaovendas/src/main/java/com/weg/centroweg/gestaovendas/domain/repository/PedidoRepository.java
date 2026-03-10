08861031bbda0dbad6d1faaed2d2220de51b7bdc
Kauã Felix <kfelixcosta11@gmail.com>
2026-03-10 20:47:32 -0300

    feat: adiciona entidade Pedido e métodos de busca no repositório

--- gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/domain/repository/PedidoRepository.java
+++ gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/domain/repository/PedidoRepository.java
@@ -3,7 +3,12 @@ package com.weg.centroweg.gestaovendas.domain.repository;
 import com.weg.centroweg.gestaovendas.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
+
+    List<Pedido> findByIdUsuario (UUID idUsuario);
+    List <Pedido> findByIdCliente (UUID idCliente);
+
 }
