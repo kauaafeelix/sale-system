305c454c510cc2c66a84e841c236d3a96ef93056
Kaua Felix <kfelixcosta11@gmail.com>
2026-03-13 16:59:32 -0300

    feat: add user role field to RegisterRequest for enhanced role management

--- gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/dto/auth/request/RegisterRequest.java
+++ gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/dto/auth/request/RegisterRequest.java
@@ -1,8 +1,12 @@
 package com.weg.centroweg.gestaovendas.application.dto.auth.request;
 
+import com.weg.centroweg.gestaovendas.domain.entity.enums.RoleUsuario;
+
 public record RegisterRequest(
         String nome,
         String email,
-        String senha
+        String senha,
+        RoleUsuario role
+
 ) {
 }
