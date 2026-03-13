77348ebdec40c4ed12c5f634b9d132efde9d516c
Kaua Felix <kfelixcosta11@gmail.com>
2026-03-13 16:59:39 -0300

    feat: add method-level security to save endpoint in UsuarioController for admin access control

--- gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/web/controller/UsuarioController.java
+++ gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/web/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import com.weg.centroweg.gestaovendas.application.service.contracts.UsuarioServi
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<UsuarioResponseDto>save(@Valid @RequestBody UsuarioRequestDto request){
 
