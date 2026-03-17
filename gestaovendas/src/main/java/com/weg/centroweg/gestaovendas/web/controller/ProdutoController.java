e99ba7b39c04ec5342367f6a47d4da5e00d7de29
Kauã Felix <kfelixcosta11@gmail.com>
2026-03-17 20:22:39 -0300

    feat: adiciona controller com os endpoints para fazer requisicoes

--- gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/web/controller/ProdutoController.java
+++ gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/web/controller/ProdutoController.java
@@ -7,22 +7,55 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/produtos")
 @RequiredArgsConstructor
 public class ProdutoController {
 
-    private final ProdutoService servive;
-
+    private final ProdutoService service;
 
     @PostMapping
-    public ResponseEntity<ProdutoResponseDto>save(@Valid @RequestBody ProdutoRequestDto request){
-        ProdutoResponseDto response = servive.criarProduto(request);
+    public ResponseEntity<ProdutoResponseDto>save(
+            @Valid @RequestBody ProdutoRequestDto request
+    ){
+        ProdutoResponseDto response = service.criarProduto(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoResponseDto>>findAll(){
+        List<ProdutoResponseDto> responses = service.listarProdutos();
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDto> findById(@PathVariable UUID id){
+        ProdutoResponseDto response = service.buscarPorId(id);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{nome}")
+    public ResponseEntity<List<ProdutoResponseDto>> findByNameContaining(@PathVariable String nome){
+        List<ProdutoResponseDto> responses = service.buscarPorNome(nome);
+
+        return ResponseEntity.ok(responses);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoResponseDto> update(
+            @PathVariable UUID id,
+            @Valid @RequestBody ProdutoRequestDto request
+    ){
+
+        ProdutoResponseDto response = service.atualizarProduto(id, request);
+
+        return ResponseEntity.ok(response);
     }
 }
