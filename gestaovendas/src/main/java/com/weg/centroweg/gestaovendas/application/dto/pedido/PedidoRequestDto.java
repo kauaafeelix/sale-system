b92567683503ac6eeb83f29e07aa9e697b0bc467
Kaua Felix <kfelixcosta11@gmail.com>
2026-03-20 00:29:54 -0300

    feat: Atualiza os campos PedidoRequestDto e PedidoResponseDto para incluir listas de itens e remover campos desnecessários.

--- gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/dto/pedido/PedidoRequestDto.java
+++ gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/dto/pedido/PedidoRequestDto.java
@@ -1,28 +1,20 @@
 package com.weg.centroweg.gestaovendas.application.dto.pedido;
 
+import com.weg.centroweg.gestaovendas.application.dto.itempedido.ItemPedidoRequestDto;
 import com.weg.centroweg.gestaovendas.domain.entity.enums.StatusPedido;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public record PedidoRequestDto(
 
         @NotNull (message = "O ID do cliente é obrigatório")
         UUID clienteId,
-
-        @NotNull (message = "O ID do usuário é obrigatório")
         UUID usuarioId,
-
-        @PastOrPresent (message = "A data do pedido não pode ser no futuro")
-        LocalDateTime dataPedido,
-
-        @Positive (message = "O valor total deve ser um número maior que zero")
-        double valorTotal,
-
-        @NotNull(message = "O status do pedido é obrigatório")
-        StatusPedido status
+        List<ItemPedidoRequestDto> itens
 ) {
 }
--- gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/dto/pedido/PedidoResponseDto.java
+++ gestaovendas/src/main/java/com/weg/centroweg/gestaovendas/application/dto/pedido/PedidoResponseDto.java
@@ -1,8 +1,10 @@
 package com.weg.centroweg.gestaovendas.application.dto.pedido;
 
+import com.weg.centroweg.gestaovendas.application.dto.itempedido.ItemPedidoResponseDto;
 import com.weg.centroweg.gestaovendas.domain.entity.enums.StatusPedido;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public record PedidoResponseDto(
@@ -11,6 +13,7 @@ public record PedidoResponseDto(
         UUID usuarioId,
         LocalDateTime dataPedido,
         double valorTotal,
-        StatusPedido status
+        StatusPedido status,
+        List<ItemPedidoResponseDto> itens
 ) {
 }
